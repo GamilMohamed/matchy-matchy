177ce20b2b0a21957c98526607c11274da66344d
Mohamed GAMIL <mgamil@bess-f2r2s11.clusters.42paris.fr>
2025-02-10 19:51:45 +0100

    ajout de tout shadcn

---
+++ client/src/components/login-form.tsx
@@ -0,0 +1,26 @@
+/*
+
+The app must allow a user to register by requesting at least their email address, username,
+last name, first name, and a password that is somehow protected. After registration, an
+email with a unique link must be sent to the registered user to verify their account.
+The user must be able to login using their username and password, and also receive
+an email allowing them to reset their password if they forget it. Additionally, the user
+must be able to log out with just one click from any page on the site.
+
+*/
+
+/*
+
+json {
+	email: string
+	firstname: string
+	lastname: string
+	password: string
+}
+
+*/
+export function LoginForm() {
+  return (
+	<div className="bg-green-400">CODE ICI </div>
+  )
+}
